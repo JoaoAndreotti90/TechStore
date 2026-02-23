f1d989c01323a638c050de47005e2a65910c8372
João Andreotti <joaogilberto238@gmail.com>
2026-02-23 11:19:29 -0300

    fix: adicionando export default no main.ts para a vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,32 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { rawBody: true });
+  if (process.env.NODE_ENV !== 'production') {
+    const app = await NestFactory.create(AppModule, { rawBody: true });
 
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+
+    const port = process.env.PORT || 3001;
+    await app.listen(port);
+    console.log(`Servidor rodando na porta: ${port}`);
+  }
+}
+bootstrap();
+
+export default async function (req: any, res: any) {
+  const app = await NestFactory.create(AppModule, { rawBody: true });
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,27 +36,13 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
-  app.enableCors({
-    origin: true,
-    credentials: true,
+  
+  app.enableCors({ 
+    origin: true, 
+    credentials: true 
   });
-
-  const port = process.env.PORT || 3001;
-  
-  if (process.env.NODE_ENV !== 'production') {
-    await app.listen(port);
-  }
-}
-bootstrap();
-
-export const handler = async (req: any, res: any) => {
-  const app = await NestFactory.create(AppModule);
-  
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({ origin: true, credentials: true });
   
   await app.init();
   const instance = app.getHttpAdapter().getInstance();
   return instance(req, res);
-};
+}
